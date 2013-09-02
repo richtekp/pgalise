d1e39255796c35906bb8202f087d96ce0a76030b
Karl-Philipp Richter <richtekp@informatik.hu-berlin.de>
2013-09-02 19:10:38 +0200

    addition

--- simulation/de.pgalise.simulation.services-impl/src/test/java/de/pgalise/simulation/service/AllTests.java
+++ simulation/de.pgalise.simulation.services-impl/src/test/java/de/pgalise/simulation/service/AllTests.java
@@ -27,7 +27,7 @@ import org.junit.runners.Suite.SuiteClasses;
  * @version 1.0 (Nov 22, 2012)
  */
 @RunWith(Suite.class)
-@SuiteClasses({ DisassemblerTest.class,
+@SuiteClasses({ 
 	EventHandlerManagerTest.class,
 	ServerConfigReaderTest.class})
 public class AllTests {
