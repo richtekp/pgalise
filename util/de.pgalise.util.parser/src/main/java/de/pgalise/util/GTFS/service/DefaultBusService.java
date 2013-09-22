b74d3f90372f7e3cd48d351d13f3687af203e10c
Karl-Philipp Richter <richtekp@informatik.hu-berlin.de>
2013-09-23 01:33:01 +0200

    changed DefaultBusService.getBusLineData to JPA

--- util/de.pgalise.util.parser/src/main/java/de/pgalise/util/GTFS/service/DefaultBusService.java
+++ util/de.pgalise.util.parser/src/main/java/de/pgalise/util/GTFS/service/DefaultBusService.java
@@ -31,15 +31,22 @@ import java.util.List;
 import de.pgalise.simulation.shared.traffic.BusRoute;
 import de.pgalise.simulation.traffic.BusStopTime;
 import de.pgalise.simulation.traffic.BusTrip;
+import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
  * @author Lena
  */
+@Singleton
 public class DefaultBusService implements BusService {
 	private final static Logger LOGGER = LoggerFactory.getLogger(DefaultBusService.class);
 	private Connection con;
+	@PersistenceContext
+	private EntityManager entityManager;
 
 	// public static void main(String[] args) {
 	// DefaultBusService bs = new DefaultBusService();
@@ -52,180 +59,10 @@ public class DefaultBusService implements BusService {
 	@Override
 	public List<BusTrip> getBusLineData(String busRoute, long timeInMs) throws ClassNotFoundException, SQLException {
 		List<BusTrip> trips = new ArrayList<>();
-
-		Class.forName("org.apache.derby.jdbc.ClientDriver");
-		con = DriverManager.getConnection("jdbc:derby://127.0.0.1:5201/database", "pgalise", "somepw");
-
-		String weekday = "monday";
-
-		Calendar cal = new GregorianCalendar();
-		cal.setTimeInMillis(timeInMs);
-		int weekdayInt = cal.get(Calendar.DAY_OF_WEEK);
-
-		switch (weekdayInt) {
-			case 1:
-				weekday = "sunday";
-				break;
-			case 2:
-				weekday = "monday";
-				break;
-			case 3:
-				weekday = "tuesday";
-				break;
-			case 4:
-				weekday = "wednesday";
-				break;
-			case 5:
-				weekday = "thursday";
-				break;
-			case 6:
-				weekday = "friday";
-				break;
-			case 7:
-				weekday = "saturday";
-				break;
-		}
-
-		String date;
-		int m = cal.get(Calendar.MONTH) + 1;
-		String month = String.valueOf(m);
-		if (m < 10) {
-			month = "0" + m;
-		}
-		int d = cal.get(Calendar.DAY_OF_MONTH);
-		String day = String.valueOf(d);
-		if (d < 9) {
-			day = "0" + d;
-		}
-		date = cal.get(Calendar.YEAR) + "-" + month + "-" + day;
-
-		// System.out.println(weekday + " " + date);
-
-		String query1 = "select distinct c.service_id from pgalise.bus_calendar c join pgalise.bus_trips t on c.service_id=t.service_id where "
-				+ weekday + "='1' and date('" + date + "')>=c.start_date and date('" + date + "')<=c.end_date";
-
-		Statement st = con.createStatement();
-		ResultSet rs = st.executeQuery(query1);
-
-		String serviceId = "";
-
-		// just get the first row in the unlikely case of getting more than one
-		// service_id
-		if (rs.next()) {
-			serviceId = rs.getString(1);
-		}
-
-		if (!serviceId.isEmpty()) {
-			// String query2 =
-			// "select distinct a.trip_id from (select r.route_id, r.route_short_name, r.route_long_name, t.trip_id from pgalise.bus_routes r join pgalise.bus_trips t on r.route_id=t.route_id where r.route_short_name = '"
-			// + busLine
-			// + "' and t.service_id='"
-			// + serviceId
-			// +
-			// "') a join pgalise.bus_stop_times st on a.trip_id=st.trip_id order by a.trip_id";
-
-			String query2 = "select distinct a.trip_id from (select r.route_id, r.route_short_name, r.route_long_name, t.trip_id from pgalise.bus_routes r join pgalise.bus_trips t on r.route_id=t.route_id where r.route_id = '"
-					+ busRoute
-					+ "' and t.service_id='"
-					+ serviceId
-					+ "') a join pgalise.bus_stop_times st on a.trip_id=st.trip_id order by a.trip_id";
-
-			st = con.createStatement();
-			rs = st.executeQuery(query2);
-
-			List<String> tripIds = new ArrayList<>();
-			while (rs.next()) {
-				tripIds.add(rs.getString("trip_id"));
-			}
-
-			for (String trip : tripIds) {
-				// String query3 =
-				// "select a.trip_id, a.route_id, a.route_short_name, a.route_long_name, st.arrival_time, st.departure_time, st.stop_id from (select r.route_id, r.route_short_name, r.route_long_name, t.trip_id from pgalise.bus_routes r join pgalise.bus_trips t on r.route_id=t.route_id where r.route_short_name = '"
-				// + busLine
-				// + "' and t.service_id='"
-				// + serviceId
-				// + "' and t.trip_id='"
-				// + trip
-				// +
-				// "') a join pgalise.bus_stop_times st on a.trip_id=st.trip_id order by a.trip_id";
-
-				String query3 = "select a.trip_id, a.route_id, a.route_short_name, a.route_long_name, st.arrival_time, st.departure_time, st.stop_id, st.stop_sequence from (select r.route_id, r.route_short_name, r.route_long_name, t.trip_id from pgalise.bus_routes r join pgalise.bus_trips t on r.route_id=t.route_id where r.route_id = '"
-						+ busRoute
-						+ "' and t.service_id='"
-						+ serviceId
-						+ "' and t.trip_id='"
-						+ trip
-						+ "') a join pgalise.bus_stop_times st on a.trip_id=st.trip_id order by st.stop_sequence";
-
-				st = con.createStatement();
-				rs = st.executeQuery(query3);
-
-				List<String> busStops = new ArrayList<>();
-				HashMap<String, BusStopTime> busStopStopTimes = new HashMap<>();
-				String tripId = "";
-				String routeId = "";
-				String routeShortName = "";
-				String routeLongName = "";
-				while (rs.next()) {
-					tripId = rs.getString("trip_id");
-					routeId = rs.getString("route_id");
-					routeShortName = rs.getString("route_short_name");
-					routeLongName = rs.getString("route_long_name");
-					Time arrivalTime = rs.getTime("arrival_time");
-					Time departureTime = rs.getTime("departure_time");
-					String stopId = rs.getString("stop_id");
-
-					busStops.add(stopId);
-					busStopStopTimes.put(stopId, new BusStopTime(null,
-						null,
-						arrivalTime,
-						departureTime,
-						weekdayInt,
-						stopId,
-						stopId,
-						stopId,
-						serviceId));
-
-					// System.out.println(tripId + "," + routeId
-					// + "," + routeShortName + "," + routeLongName + ","
-					// + arrivalTime + "," + departureTime + ","
-					// + stopId);
-				}
-
-				if (!(tripId.isEmpty() && routeId.isEmpty() && routeShortName.isEmpty() && routeLongName.isEmpty() && busStopStopTimes
-						.size() > 0)) {
-					BusTrip busTrip = new BusTrip(tripId, routeId, routeShortName, routeLongName, busStops,
-							busStopStopTimes, null);
-					trips.add(busTrip);
-					// System.out.println("Trip " + tripId + " added with " +
-					// busStopStopTimes.size() + " stoptimes");
-				}
-			}
-
-			/*
-			 * String query3 =
-			 * "select a.trip_id, a.route_id, a.route_short_name, a.route_long_name, st.arrival_time, st.departure_time, st.stop_id from (select r.route_id, r.route_short_name, r.route_long_name, t.trip_id from pgalise.bus_routes r join pgalise.bus_trips t on r.route_id=t.route_id where r.route_short_name = '"
-			 * + busLine + "' and t.service_id='" + serviceId +
-			 * "') a join pgalise.bus_stop_times st on a.trip_id=st.trip_id order by a.trip_id" ; st =
-			 * con.createStatement(); rs = st.executeQuery(query3); String lastTripId = ""; HashMap<String,
-			 * BusStopTimes> busStopStopTimes = new HashMap<>(); while (rs.next()) { String tripId =
-			 * rs.getString("trip_id"); String routeId = rs.getString("route_id"); String routeShortName =
-			 * rs.getString("route_short_name"); String routeLongName = rs.getString("route_long_name"); Time
-			 * arrivalTime = rs.getTime("arrival_time"); Time departureTime = rs.getTime("departure_time"); String
-			 * stopId = rs.getString("stop_id"); if (lastTripId.isEmpty() || !tripId.equals(lastTripId)) { if
-			 * (busStopStopTimes.size() > 0) { BusTrip trip = new BusTrip(tripId, routeId, routeShortName,
-			 * routeLongName, busStopStopTimes); trips.add(trip); System.out.println("New trip added: " + tripId);
-			 * busStopStopTimes.clear(); } } busStopStopTimes.put(stopId, new BusStopTimes(arrivalTime, departureTime));
-			 * System.out.println(tripId + "," + routeId + "," + routeShortName + "," + routeLongName + "," +
-			 * arrivalTime + "," + departureTime + "," + stopId); lastTripId = tripId; }
-			 */
-		}
-
-		rs.close();
-		st.close();
-		con.close();
-
-		return trips;
+		TypedQuery<BusTrip> query = entityManager.createNamedQuery("SELECT x FROM BusTrip x",
+			BusTrip.class);
+		List<BusTrip> retValue = query.getResultList();
+		return retValue;
 	}
 
 	@Override
@@ -357,6 +194,8 @@ public class DefaultBusService implements BusService {
 	@Override
 	public List<BusRoute> getAllBusRoutes() throws ClassNotFoundException, SQLException {
 		List<BusRoute> routes = new ArrayList<>();
+		
+		
 
 		Class.forName("org.apache.derby.jdbc.ClientDriver");
 		con = DriverManager.getConnection("jdbc:derby://127.0.0.1:5201/database", "pgalise", "somepw");
