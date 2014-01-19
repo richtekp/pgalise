94b4edf76cb4fe7ee7c4302dcea58b840353d5bf
Karl-Philipp Richter <richtekp@informatik.hu-berlin.de>
2014-01-19 19:29:48 +0100

    fixed InitParameter and StartParameter inheritance

--- simulation/de.pgalise.simulation.weather/src/main/java/de/pgalise/simulation/weather/service/WeatherInitParameter.java
+++ simulation/de.pgalise.simulation.weather/src/main/java/de/pgalise/simulation/weather/service/WeatherInitParameter.java
@@ -2,29 +2,37 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ *//*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package de.pgalise.simulation.weather.service;
 
 import com.vividsolutions.jts.geom.Envelope;
-import de.pgalise.simulation.service.InitParameter;
 import de.pgalise.simulation.service.SimulationInitParameter;
-import de.pgalise.simulation.shared.controller.TrafficFuzzyData;
 import de.pgalise.simulation.shared.entity.City;
 import java.net.URL;
 
 /**
  *
  * @author richter
+ * @param <C>
  */
-public class WeatherInitParameter extends SimulationInitParameter {
-	private static final long serialVersionUID = 1L;
+public class WeatherInitParameter<C extends City> extends SimulationInitParameter {
 
-  private City city;
+  private static final long serialVersionUID = 1L;
 
-	public WeatherInitParameter() {
-	}
+  private C city;
 
-  public WeatherInitParameter(City city,
+  public WeatherInitParameter() {
+  }
+
+  public WeatherInitParameter(C city) {
+    this.city = city;
+  }
+
+  public WeatherInitParameter(C city,
     long startTimestamp,
     long endTimestamp,
     long interval,
@@ -42,11 +50,11 @@ public class WeatherInitParameter extends SimulationInitParameter {
     this.city = city;
   }
 
-  public void setCity(City city) {
+  public void setCity(C city) {
     this.city = city;
   }
 
-  public City getCity() {
+  public C getCity() {
     return city;
   }
 }
